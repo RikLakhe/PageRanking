ebe576593474d07a232d36aa89e173fe4b4dfc69
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-05-29 00:45:10 +0545

    first part is done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PageRanking
 Python code web spider 
+
+Capstone Page-rank
+PYTHON with URLLIB, SQLITE, BEAUTIFULSOUP
+
+![imgfirst](/images/img1.png)
--- spider.py
+++ spider.py
@@ -60,7 +60,7 @@ class spider:
             if len(numberPages) < 1: break
             many = int(numberPages)
         many = many - 1
-        print(many)
+        # print(many)
 
         # part 5 take a url from pages
         try:
@@ -68,7 +68,7 @@ class spider:
             row = cur.fetchone()
             from_id = row[0]
             url = row[1]
-            print('here', from_id, url)
+            # print('here', from_id, url)
             # remove all the link from this page in link
             cur.execute('delete from Links where from_id = ?', (from_id,))
         except:
@@ -76,13 +76,74 @@ class spider:
             many = 0
             break
 
+        print(from_id, url, end=' ')
+
         # part 6 urlopen the url and get response from web
         try:
             document = urlopen(url, context=ctx)
-            print('retreve data', document.getcode(), document.info())
+            # print('response data', document.getcode(), document.info().get_content_type())
+            if document.getcode() != 200:
+                print("Error on page")
+                cur.execute('update Pages set error=? where url=?', (document.getcode(), url,))
+                continue
+            if document.info().get_content_type() != 'text/html':
+                print("Ignore non text/html page")
+                cur.execute('delete from Pages where url=?', (url,))
+                continue
 
+            html = document.read()
+            print('(' + str(len(html)) + ')', end=' ')
+            soup = BeautifulSoup(html, 'html.parser')
         except:
             print("Unable to retrieve or parse page")
             cur.execute('update Pages set error=-1 where url=?', (url,))
             con.commit()
-            break
+            continue
+
+        cur.execute('insert or ignore into Pages (url,html,new_rank) values (?,null,1.0)', (url,))
+        cur.execute('update Pages set html=? where url=?', (memoryview(html), url,))
+        # part 7 going through a tags to get href
+        count = 0
+        tags = soup('a')
+        for tag in tags:
+            href = tag.get('href', None)
+
+            if href is None: continue
+            # handling /content
+            up = urlparse(href)
+            if len(up.scheme) < 1: href = urljoin(url, href)
+            # handling "#"
+            ipos = href.find("#")
+            if ipos > 1: href = href[:ipos]
+            if href.endswith('.png') or href.endswith('.gif') or href.endswith('.jpg') or href.endswith(
+                    '.jpeg'): continue
+            if href.endswith("/"): href = href[:-1]
+            if len(href) < 1: continue
+
+            # print('href got is ', href)
+
+            # Check if the URL is in any of the webs
+            found = False
+            for web in webs:
+                if (href.startswith(web)):
+                    found = True
+                    break
+            if not found: continue
+
+            cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', (href,))
+            count = count + 1
+            con.commit()
+
+            # part 7 add in links from id to to id
+            cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', (href,))
+            try:
+                row = cur.fetchone()
+                to_id = row[0]
+            except:
+                print('Could not retrieve id')
+                continue
+            cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', (from_id, to_id))
+
+        print(count)
+
+    cur.close()
